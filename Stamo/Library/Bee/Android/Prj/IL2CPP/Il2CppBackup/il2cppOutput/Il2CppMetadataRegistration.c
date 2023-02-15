3246c3d8d10f6d92e6214ab7a5c16e9584871ddc
Merlan <merlan01tel@gmail.com>
2023-02-15 23:28:48 +0600

    Update #3

---
+++ Stamo/Library/Bee/Android/Prj/IL2CPP/Il2CppBackup/il2cppOutput/Il2CppMetadataRegistration.c
@@ -0,0 +1,41 @@
+﻿#include "pch-c.h"
+#ifndef _MSC_VER
+# include <alloca.h>
+#else
+# include <malloc.h>
+#endif
+
+
+#include "codegen/il2cpp-codegen-metadata.h"
+
+
+
+
+
+extern Il2CppGenericClass* const g_Il2CppGenericTypes[];
+extern const Il2CppGenericInst* const g_Il2CppGenericInstTable[];
+extern const Il2CppGenericMethodFunctionsDefinitions g_Il2CppGenericMethodFunctions[];
+extern const Il2CppType* const  g_Il2CppTypeTable[];
+extern const Il2CppMethodSpec g_Il2CppMethodSpecTable[];
+IL2CPP_EXTERN_C_CONST int32_t* g_FieldOffsetTable[];
+IL2CPP_EXTERN_C_CONST Il2CppTypeDefinitionSizes* g_Il2CppTypeDefinitionSizesTable[];
+IL2CPP_EXTERN_C const Il2CppMetadataRegistration g_MetadataRegistration;
+const Il2CppMetadataRegistration g_MetadataRegistration = 
+{
+	5870,
+	g_Il2CppGenericTypes,
+	4118,
+	g_Il2CppGenericInstTable,
+	53891,
+	g_Il2CppGenericMethodFunctions,
+	21262,
+	g_Il2CppTypeTable,
+	62750,
+	g_Il2CppMethodSpecTable,
+	6905,
+	g_FieldOffsetTable,
+	6905,
+	g_Il2CppTypeDefinitionSizesTable,
+	0,
+	NULL,
+};
